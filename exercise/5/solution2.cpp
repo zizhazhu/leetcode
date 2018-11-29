0444816f170e05f266c1780981a25a07624e90ce
zizhazhu <zizhazhu@gmail.com>
2018-11-29 10:08:21 +0800

    fix bugs

--- exercise/5/solution2.cpp
+++ exercise/5/solution2.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         vector<vector<int>> f(s.length(), vector<int>(s.length(), 0));
+		int max_length = 0, left = 0;
 		for (int i = 0; i < s.length(); i++) {
 			f[i][i] = 1;
+			if (max_length < 1) {
+				max_length = 1;
+				left = i;
+			}
 		}
 		for (int i = 0; i < s.length() - 1; i++) {
 			if (s[i] == s[i+1]) {
 				f[i][i+1] = 2;
+				if (max_length < 2) {
+					max_length = 2;
+					left = i;
+				}
 			}
 		}
-		int max_length = 0, left = 0;
 		for (int l = 2; l < s.length(); l++) {
 			for (int i = 0; i + l < s.length(); i++) {
 				if (s[i] == s[i + l]) {
@@ -24,7 +32,7 @@ public:
 				}
 			}
 		}
-		return s.substr(left, max_length + 1);
+		return s.substr(left, max_length);
     }
 };
 
