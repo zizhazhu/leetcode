fcd679f382f80e7e099a929051f4c547ce48f8e5
zizhazhu <zizhazhu@gmail.com>
2018-12-01 11:52:55 +0800

    fix bugs

--- exercise/5/manacher.cpp
+++ exercise/5/manacher.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-		int max_length = 1, pos = 0;
+		int max_length = 0, pos = 0;
         string p_s;
 		p_s.push_back('#');
 		for (int i = 0; i < s.length(); i++) {
@@ -13,23 +13,27 @@ public:
 		for (int i = 1; i < p_s.length(); i++) {
 			if (i >= right) {
 				center = i;
-				for (int l = 0; i - l >= 0 && i + l < s.length(); l++) {
+				for (int l = 0; i - l >= 0 && i + l < p_s.length(); l++) {
 					if (p_s[i-l] == p_s[i+l]) {
 						right = i + l;
 						longest[i] = l;
-					}
+					} else {
+                        break;
+                    }
 				}
 			} else {
 				if (longest[2 * center - i] < right - i) {
 					longest[i] = longest[2 * center - i];
-				} else if (longest[2 * center -i] == right - i) {
+				} else if (longest[2 * center - i] == right - i) {
 					longest[i] = right - i;
 					center = i;
-					for (int l = longest[i] + 1; i - l >= 0 && i + l < s.length(); l++) {
+					for (int l = longest[i] + 1; i - l >= 0 && i + l < p_s.length(); l++) {
 						if (p_s[i-l] == p_s[i+l]) {
 							right = i + l;
 							longest[i] = l;
-						}
+						} else {
+                            break;
+                        }
 					}
 				} else {
 					longest[i] = right - i;
