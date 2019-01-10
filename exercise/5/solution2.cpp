2fe77faff9904fc1ed0b023a6a42861c99327d45
zizhazhu <zizhazhu@gmail.com>
2019-01-10 11:48:29 +0800

    update c version solution2

--- exercise/5/solution2.cpp
+++ exercise/5/solution2.cpp
@@ -7,30 +7,12 @@ public:
         vector<vector<int>> f(s.length(), vector<int>(s.length(), 0));
 		int max_length = 0, left = 0;
 		for (int i = 0; i < s.length(); i++) {
-			f[i][i] = 1;
-			if (max_length < 1) {
-				max_length = 1;
-				left = i;
-			}
-		}
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s[i] == s[i+1]) {
-				f[i][i+1] = 2;
-				if (max_length < 2) {
-					max_length = 2;
-					left = i;
-				}
-			}
-		}
-		for (int l = 2; l < s.length(); l++) {
-			for (int i = 0; i + l < s.length(); i++) {
-				if (s[i] == s[i + l]) {
-					if (f[i+1][i+l-1] != 0) {
-						f[i][i+l] = f[i+1][i+l-1] + 2;
-						if (max_length < f[i][i+l]) {
-							max_length = f[i][i+l];
-							left = i;
-						}
+			for (int j = i; j >= 0; j--) {
+				if (s[j] == s[i] && (i - j < 2 || f[j+1][i-1] == 1)) {
+					f[j][i] = 1;
+					if (max_length < i - j) {
+						max_length = i - j;
+						left = j;
 					}
 				}
 			}
