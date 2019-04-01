d674be07d41702fd39cf3e6629e78e411faf92cb
zizhazhu <zizhazhu@gmail.com>
2019-04-01 11:29:24 +0800

    add exercise 8 solution

---
+++ exercise/8/solution.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        if (str.length() == 0) {
+            return 0;
+        }
+        int state = 0, pos = 0, sign = 1, num = 0, overflow = 0;
+        while (true) {
+            switch (state) {
+                case 0:
+                    if (str[pos] == ' ') {
+                        ;
+                    } else if (str[pos] == '+') {
+                        state = 1;
+                    } else if (str[pos] == '-') {
+                        sign = -1;
+                        state = 1;
+                    } else if (str[pos] >= '0' && str[pos] <= '9') {
+                        num = str[pos] - '0';
+                    } else {
+                        state = 2;
+                    }
+                    break;
+                case 1:
+                    if (str[pos] >= '0' && str[pos] <= '9') {
+                        int now = str[pos] - '0';
+                        if (sign == -1) {
+                            if (num == 214748364 && now >= 8 || num > 214748364) {
+                                state = 2;
+                                overflow = 1;
+                            } else {
+                                num *= 10;
+                                num += now;
+                            }
+                        } else {
+                            if (num == 214748364 && now >= 7 || num > 214748364) {
+                                state = 2;
+                                overflow = 1;
+                            } else {
+                                num *= 10;
+                                num += now;
+                            }
+                        }
+                    } else {
+                        state = 2;
+                    }
+                case 2:
+                    if (overflow == 1) {
+                        if (sign == 1) {
+                            return 2147483647;
+                        } else {
+                            return -2147483648;
+                        }
+                    } else {
+                        return sign * num;
+                    }
+            }
+            pos++;
+        }
+    }
+};
