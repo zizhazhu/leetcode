aefd8088549574a67f599073f83700b6521871c8
zizhazhu <zizhazhu@gmail.com>
2019-04-02 15:51:35 +0800

    add exercise 12 cpp solution

---
+++ exercise/12/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman = 'IVXLCDM';
+        string result;
+        int tnum = num, pos = 0;
+        while (tnum) {
+            string delta = '';
+            int digit = tnum % 10;
+            tnum /= 10;
+            switch (digit) {
+                case 9:
+                    delta = roman[pos] + roman[pos+2];
+                    break;
+                case 8:
+                    delta += roman[pos];
+                case 7:
+                    delta += roman[pos];
+                case 6:
+                    delta += roman[pos];
+                case 5:
+                    delta += roman[pos+1];
+                    break;
+                case 4:
+                    delta = roman[pos] + roman[pos+1];
+                    break;
+                case 3:
+                    delta += roman[pos];
+                case 2:
+                    delta += roman[pos];
+                case 1:
+                    delta += roman[pos];
+            }
+            result = delta + result;
+            pos += 2;
+        }
+        return result;
+    }
+};
