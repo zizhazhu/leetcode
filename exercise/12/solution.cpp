b2b7a361779d6b1a324b5a9f16619039150c91bb
zizhazhu <zizhazhu@gmail.com>
2019-04-02 16:04:12 +0800

    fix bug

--- exercise/12/solution.cpp
+++ exercise/12/solution.cpp
@@ -10,26 +10,28 @@ public:
             tnum /= 10;
             switch (digit) {
                 case 9:
-                    delta = "" + roman[pos] + roman[pos+2];
+                    delta.push_back(roman[pos]);
+                    delta.push_back(roman[pos+2]);
                     break;
                 case 8:
-                    delta += roman[pos];
+                    delta.push_back(roman[pos]);
                 case 7:
-                    delta += roman[pos];
+                    delta.push_back(roman[pos]);
                 case 6:
-                    delta += roman[pos];
+                    delta.push_back(roman[pos]);
                 case 5:
                     delta = roman[pos+1] + delta;
                     break;
                 case 4:
-                    delta = "" + roman[pos] + roman[pos+1];
+                    delta.push_back(roman[pos]);
+                    delta.push_back(roman[pos+1]);
                     break;
                 case 3:
-                    delta += roman[pos];
+                    delta.push_back(roman[pos]);
                 case 2:
-                    delta += roman[pos];
+                    delta.push_back(roman[pos]);
                 case 1:
-                    delta += roman[pos];
+                    delta.push_back(roman[pos]);
             }
             result = delta + result;
             pos += 2;
