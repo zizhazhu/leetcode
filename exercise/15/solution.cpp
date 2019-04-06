31e0c46e85607baf5325df7b89be8704208097a6
zizhazhu <zizhazhu@gmail.com>
2019-04-06 12:53:33 +0800

    fix bug

--- exercise/15/solution.cpp
+++ exercise/15/solution.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        for (int i = 0; i < nums.size() - 2; i++) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < (int)nums.size() - 2; i++) {
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
             int left = i + 1, right = nums.size() - 1;
             while (left < right) {
-                if (nums[i] + nums[left] + nums[right] == 0) {
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
                     vector<int> now;
                     now.push_back(nums[i]);
                     now.push_back(nums[left]);
                     now.push_back(nums[right]);
                     result.push_back(now);
+                    while (left < right && nums[right] == nums[right-1])
+                        right--;
+                    while (left < right && nums[left] == nums[left+1])
+                        left++;
                     left++;
                     right--;
-                }
-                while (nums[i] + nums[left] + nums[right] > 0 || (right < nums.size() - 1 && nums[right] ==  nums[right+1])) {
-                    right--;
-                }
-                while (nums[i] + nums[left] + nums[right] < 0 || (left > i + 1 && nums[left] ==  nums[left-1])) {
-                    left++;
                 }
             }
         }
