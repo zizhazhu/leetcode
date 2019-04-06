10cc2d6787897d9b74104c9227e7d497179717b1
zizhazhu <zizhazhu@gmail.com>
2019-04-06 12:42:01 +0800

    rewrite cpp solution

--- exercise/15/solution.cpp
+++ exercise/15/solution.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        map<int, vector<pair<int, int>>> two_sum;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                int sum = nums[i] + nums[j];
-                two_sum[sum].push_back(make_pair(i, j));
-            }
-        }
         vector<vector<int>> result;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            vector<pair<int, int>> &target = two_sum[-nums[i]];
-            for (int j = 0; j < target.size(); j++) {
-                if (target[j].first > i && target[j].second > i) {
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
                     vector<int> now;
                     now.push_back(nums[i]);
-                    now.push_back(nums[target[j].first]);
-                    now.push_back(nums[target[j].second]);
+                    now.push_back(nums[left]);
+                    now.push_back(nums[right]);
                     result.push_back(now);
+                    left++;
+                    right--;
+                }
+                while (nums[i] + nums[left] + nums[right] > 0 || (right < nums.size() - 1 && nums[right] ==  nums[right+1])) {
+                    right--;
+                }
+                while (nums[i] + nums[left] + nums[right] < 0 || (left > i + 1 && nums[left] ==  nums[left-1])) {
+                    left++;
                 }
             }
         }
-        vector<vector<int>>::iterator iter = unique(result.begin(), result.end());
-        result.erase(iter, result.end());
         return result;
     }
 };
