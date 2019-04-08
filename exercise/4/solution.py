0fc9dee9e74dd4b028e7be933c4f63d954c266ef
zizhazhu <zizhazhu@gmail.com>
2019-04-08 18:52:43 +0800

    fix bug

--- exercise/4/solution.py
+++ exercise/4/solution.py
@@ -7,19 +7,19 @@ class Solution:
         while left < right:
             i = (left + right + 1) // 2
             j = (m + n + 1) // 2 - i
-            if (i == 0 or nums1[i-1] == nums2[j]) and (i == m or nums1[i] >= nums2[j-1]):
+            if (i == 0 or nums1[i-1] <= nums2[j]) and (i == m or nums1[i] >= nums2[j-1]):
                 if i == 0:
                     mid_left = nums2[j-1]
                 elif j == 0:
                     mid_left = nums1[i-1]
                 else:
-                    mid_left = max(nums[i-1], nums[j-1])
+                    mid_left = max(nums1[i-1], nums2[j-1])
                 if i == m and i != 0:
                     mid_right = nums2[j]
                 elif j == n:
                     mid_right = nums1[i]
                 else:
-                    mid_right = min(nums1[i], nums[j])
+                    mid_right = min(nums1[i], nums2[j])
                 if (m + n) % 2 == 0:
                     return (mid_left + mid_right) / 2
                 else:
