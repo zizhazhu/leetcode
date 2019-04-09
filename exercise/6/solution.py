306fbca23190d27f1ea251800f9db6e2e6a4034c
zizhazhu <zizhazhu@gmail.com>
2019-04-09 20:32:19 +0800

    exercise 6 add python solution

---
+++ exercise/6/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        gap = numRows * 2 - 2
+        result = ""
+        for i in range(0, len(s), gap):
+            result += s[i]
+        for i in range(1, numRows):
+            for j in range(i, len(s), gap):
+                result += s[j]
+                if j + gap - 2 * i < len(s):
+                    result += s[j + gap - 2 * i]
+        for i in range(numRows - 1, len(s), gap):
+            result += s[i]
+        return result
