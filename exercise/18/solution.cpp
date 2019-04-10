3aa00639faaa7f007aa0f2f15076846ed9ef89ed
zizhazhu <zizhazhu@gmail.com>
2019-04-10 10:51:59 +0800

    fix bug

--- exercise/18/solution.cpp
+++ exercise/18/solution.cpp
@@ -17,17 +17,17 @@ public:
                     int sum = nums[i] + nums[j] + nums[left] + nums[right];
                     if (sum == target) {
                         result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left+1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right-1]) {
+                            right--;
+                        }
                         left++;
                         right--;
                     } else if (sum < target) {
                         left++;
                     } else {
-                        right--;
-                    }
-                    while (left < right && nums[left] == nums[left-1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right+1]) {
                         right--;
                     }
                 }
