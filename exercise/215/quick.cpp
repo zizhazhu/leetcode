634fed8b941c0ac9d84e6b2f3860cd825f2e7181
zizhazhu <zizhazhu@gmail.com>
2019-04-11 15:07:34 +0800

    exercise 215 add cpp solution quick

---
+++ exercise/215/quick.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void swap(vector<int> &nums, int a, int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+
+    int quick(vector<int> &nums, int k, int begin, int end) {
+        int q = nums[end-1];
+        int left = begin;
+        for (int i = begin; i < end - 1; i++) {
+            if (nums[i] < q) {
+                swap(nums, i, left);
+                left++;
+            }
+        }
+        swap(nums, end - 1, left);
+        if (left == k) {
+            return nums[left];
+        } else if (left < k) {
+            return quick(nums, k - left - 1, left + 1, end);
+        } else {
+            return quick(nums, k, 0, left);
+        }
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        return quick(nums, k - 1, 0, nums.size());
+    }
+};
