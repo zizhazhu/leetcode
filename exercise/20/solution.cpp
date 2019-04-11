479aee7bdb597b68832e3b4884d168769dff519e
zizhazhu <zizhazhu@gmail.com>
2019-04-11 14:27:40 +0800

    exercise 20 add cpp solution

---
+++ exercise/20/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() == 0) {
+            return true;
+        }
+        stack<char> bracket;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                bracket.push(s[i]);
+            } else if (s[i] == ')') {
+                if (bracket.top() == '(') {
+                    bracket.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == ']') {
+                if (bracket.top() == '[') {
+                    bracket.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == '}') {
+                if (bracket.top() == '{') {
+                    bracket.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
