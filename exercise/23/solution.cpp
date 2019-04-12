e3d6dba7083969f0063f2b1347b924c4af0dc243
zizhazhu <zizhazhu@gmail.com>
2019-04-12 14:04:37 +0800

    exercise 23 add cpp solution

---
+++ exercise/23/solution.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // a fake head
+        ListNode *head = ListNode(0);
+        ListNode *tail = head;
+        int last = -2147483648;
+        bool exist = true;
+        while (exist) {
+            exist = false;
+            int min = 2147483647, no = -1;
+            for (int i = 0; i < lists.size(); i++) {
+                if (lists[i]) {
+                    exist = true;
+                    if (lists[i]->val == last) {
+                        tail->next = lists[i];
+                        lists[i] = lists[i]->next;
+                        tail = tail->next;
+                        tail->next = NULL;
+                    } else {
+                        if (lists[i]->val < min) {
+                            min = lists[i]->val;
+                            no = i;
+                        }
+                    }
+                }
+            }
+            if (no != -1) {
+                tail->next = lists[no];
+                lists[no] = lists[no]->next;
+                tail = tail->next;
+                tail->next = NULL;
+                last = min;
+            }
+        }
+        return head->next;
+    }
+};
