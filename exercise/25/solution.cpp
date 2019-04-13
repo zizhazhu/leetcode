38af2ebdb29b551947cf8747be8f68dbde228c5a
zizhazhu <zizhazhu@gmail.com>
2019-04-13 15:44:55 +0800

    exercise add cpp solution

---
+++ exercise/25/solution.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (k == 1) {
+            return head;
+        }
+        ListNode fake = ListNode(0);
+        ListNode *point = &fake;
+        while (true) {
+            ListNode *last = point, *first;
+            bool done = false;
+            for (int i = 0; i < k; i++) {
+                if (last->next) {
+                    last = last->next;
+                } else {
+                    done = true;
+                    break;
+                }
+            }
+            if (done)
+                break;
+            last = point->next;
+            first = last->next;
+            for (int i = 0; i < k - 1; i++) {
+                last->next = first->next;
+                first->next = point->next;
+                point->next = first;
+            }
+            point = last;
+        }
+        return fake.next;
+    }
+};
