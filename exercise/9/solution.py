6eabd6a7e1013440cf1fe60a37ed1224edc52fb7
zizhazhu <zizhazhu@gmail.com>
2019-04-14 12:45:14 +0800

    fix bug

--- exercise/9/solution.py
+++ exercise/9/solution.py
@@ -5,5 +5,5 @@ class Solution:
         reversed_num = 0
         while x > reversed_num:
             reversed_num = reversed_num * 10 + x % 10
-            x /= 10
-        return x == reversed_num or x == reversed_num / 10
+            x //= 10
+        return x == reversed_num or x == reversed_num // 10
