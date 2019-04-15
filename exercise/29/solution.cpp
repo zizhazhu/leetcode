92666dd6b4a9e3a21cf3ebec26c1bab8d2e1b8d1
zizhazhu <zizhazhu@gmail.com>
2019-04-15 20:33:21 +0800

    exercise 29 add app solution

--- exercise/29/solution.cpp
+++ exercise/29/solution.cpp
@@ -6,16 +6,46 @@ public:
             return 0;
         } else if (dividend == -2147483648 and divisor == -1) {
             return 2147483647;
-        } else if (dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0) {
-            while (abs(dividend) >= abs(divisor)) {
+        }
+        if (divisor == -2147483648) {
+            if (dividend == -2147483648) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (dividend == -2147483648) {
+            if (divisor < 0) {
                 dividend -= divisor;
                 result++;
-            }
-        } else if (dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0) {
-            while (abs(dividend) >= abs(divisor)) {
+            } else {
                 dividend += divisor;
                 result--;
             }
+        }
+        int sign;
+        if (dividend < 0 && divisor < 0 || dividend > 0 && divisor > 0) {
+            sign = 1;
+        } else {
+            sign = -1;
+        }
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+        if (dividend < divisor) {
+            return result;
+        }
+        int factor = 1, mul_divisor = divisor;
+        while ((mul_divisor < (1 << 30)) && (mul_divisor << 1) <= dividend) {
+            mul_divisor <<= 1;
+            factor <<= 1;
+        }
+        while (dividend >= divisor) {
+            if (dividend >= mul_divisor) {
+                dividend -= mul_divisor;
+                result += sign * factor;
+            }
+            mul_divisor >>= 1;
+            factor >>= 1;
         }
         return result;
     }
