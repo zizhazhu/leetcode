5cc223954e51448bdeac96dc03d0d04a3b0564d2
zizhazhu <zizhazhu@gmail.com>
2019-04-16 11:43:58 +0800

    exercise 30 add cpp solution

---
+++ exercise/30/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        if (words.empty()) {
+            return result;
+        }
+        int length = words[0].length();
+        int total_length = length * words.size();
+        if (total_length > s.length()) {
+            return result;
+        }
+        for (int i = 0; i + total_length <= s.length(); i++) {
+            map<string, int> words_hash;
+            for (int j = 0; j < words.size(); j++) {
+                words_hash[words[j]] = 0;
+            }
+            bool all = true;
+            for (int j = 0; i + j * length < i + total_length; j++) {
+                string part = s.substr(i + j * length, length);
+                if (words_hash.find(part) != words_hash.end() && words_hash[part] == 0) {
+                    words_hash[part] = 1;
+                } else {
+                    all = false;
+                    break;
+                }
+            }
+            if (all) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
