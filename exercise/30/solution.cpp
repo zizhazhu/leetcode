b68e2d38291ec78d14bbe778698d14e6f3f29bb5
zizhazhu <zizhazhu@gmail.com>
2019-04-16 16:13:19 +0800

    fix bug

--- exercise/30/solution.cpp
+++ exercise/30/solution.cpp
@@ -13,13 +13,13 @@ public:
         for (int i = 0; i + total_length <= s.length(); i++) {
             map<string, int> words_hash;
             for (int j = 0; j < words.size(); j++) {
-                words_hash[words[j]] = 0;
+                words_hash[words[j]]++;
             }
             bool all = true;
             for (int j = 0; i + j * length < i + total_length; j++) {
                 string part = s.substr(i + j * length, length);
-                if (words_hash.find(part) != words_hash.end() && words_hash[part] == 0) {
-                    words_hash[part] = 1;
+                if (words_hash.find(part) != words_hash.end() && words_hash[part] != 0) {
+                    words_hash[part]--;
                 } else {
                     all = false;
                     break;
