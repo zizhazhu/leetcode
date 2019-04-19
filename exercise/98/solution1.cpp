8fa831895b054db1cd53cefb789d03c9810a01c0
zizhazhu <zizhazhu@gmail.com>
2019-04-19 11:34:54 +0800

    exercise 98 add cpp solution1

---
+++ exercise/98/solution1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<pair<TreeNode*, int>> nodes;
+        nodes.push(make_pair(root, 0));
+        int value = INT_MIN;
+        while (!nodes.empty()) {
+            pair<TreeNode*, int> now = nodes.top();
+            nodes.pop();
+            TreeNode *node = now.first;
+            if (!node) continue;
+            if (now->second == 0) {
+                nodes.push(make_pair(node, 1));
+                nodes.push(make_pair(node->left, 0));
+            } else if (now->second == 1) {
+                if (node->val <= value)
+                    return false;
+                else
+                    value = node->val;
+                nodes.push(make_pair(node->right, 0));
+            }
+        }
+        return true;
+    }
+};
