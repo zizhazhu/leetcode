46179da389f9799bd550a67a0b680de53f9f88c0
zizhazhu <zizhazhu@gmail.com>
2019-04-22 20:53:14 +0800

    fix bug

--- exercise/15/solution.py
+++ exercise/15/solution.py
@@ -7,13 +7,13 @@ class Solution:
                 continue
             left, right = i + 1, len(nums) - 1
             while left < right:
-                while left < right - 1 and nums[left] == nums[left+1]:
-                    left += 1
-                while left < right - 1 and nums[right] == nums[right-1]:
-                    right -= 1
                 now = nums[i] + nums[left] + nums[right]
                 if now == 0:
                     result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
                     left += 1
                     right -= 1
                 elif now < 0:
