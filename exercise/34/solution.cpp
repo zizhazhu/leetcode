bdf76e8e22fdf443a4e24aafc3aab252c790e9c9
zizhazhu <zizhazhu@gmail.com>
2019-04-23 18:05:57 +0800

    exercise 34 add cpp solution

--- exercise/34/solution.cpp
+++ exercise/34/solution.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        
+        int target_l = -1, target_r = -1;
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (nums[left] == target) {
+            target_l = left;
+        }
+        left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] == target) {
+                left = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (nums[left] == target) {
+            target_r = left;
+        }
+        return vector<int>{target_l, target_r};
     }
 };
