7205e897569d5255484175b6ab7047873f2df68c
zizhazhu <zizhazhu@gmail.com>
2019-04-25 10:26:22 +0800

    exercise 18 add python solution

---
+++ exercise/18/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
+                left, right = j + 1, len(nums) - 1
+                while left < right:
+                    now = nums[i] + nums[j] + nums[left] + nums[right]
+                    if now < target:
+                        left += 1
+                    elif now > target:
+                        right -= 1
+                    else:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right+1]:
+                            right -= 1
+        return result
