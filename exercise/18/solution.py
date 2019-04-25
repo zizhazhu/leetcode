273b9ddebaff5751f4489504215f6a83ea43974a
zizhazhu <zizhazhu@gmail.com>
2019-04-25 10:44:04 +0800

    optimize

--- exercise/18/solution.py
+++ exercise/18/solution.py
@@ -3,9 +3,17 @@ class Solution:
         result = []
         nums.sort()
         for i in range(len(nums) - 3):
+            if nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target:
+                break
+            if nums[i] + nums[len(nums)-1] + nums[len(nums)-2] + nums[len(nums)-3] < target:
+                continue
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i + 1, len(nums) - 2):
+                if nums[i] + nums[j] + nums[j+1] + nums[j+2] > target:
+                    break
+                if nums[i] + nums[j] + nums[len(nums)-1] + nums[len(nums)-2] < target:
+                    continue
                 if j > i + 1 and nums[j] == nums[j-1]:
                     continue
                 left, right = j + 1, len(nums) - 1
