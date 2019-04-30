af220940dd506e31d5b72eb6e0a6fa7f7f40e3aa
zizhazhu <zizhazhu@gmail.com>
2019-04-30 11:31:14 +0800

    fix bug

--- exercise/41/solution.cpp
+++ exercise/41/solution.cpp
@@ -11,6 +11,7 @@ public:
             if (nums[i] <= 0 || nums[i] > nums.size() || nums[i] == i + 1)
                 continue;
             swap(nums, i, nums[i] - 1);
+            i--;
         }
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != i - 1)
