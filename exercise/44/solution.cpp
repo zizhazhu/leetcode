5210b2766591e7bafac6cad39bdf10e27afd0195
zizhazhu <zizhazhu@gmail.com>
2019-05-02 22:19:14 +0800

    exercise 44 add cpp solution

---
+++ exercise/44/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<bool>> match(s.length() + 1, vector<bool>(p.length() + 1, false));
+        match[0][0] = true;
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (i > 0 && match[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '?' || p[j-1] == '*')) {
+                    match[i][j] = true;
+                }
+                if (match[i][j-1] && p[j-1] == '*') {
+                    match[i][j] = true;
+                }
+            }
+        }
+        return match[s.length()][p.length()];
+    }
+};
