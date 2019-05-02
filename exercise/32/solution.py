86dfdc23dab3d3544c0ae92af67eea0efba55b48
zizhazhu <zizhazhu@gmail.com>
2019-05-02 16:03:42 +0800

    exercise 32 add python solution

---
+++ exercise/32/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        result = 0
+        valid = [-1 for i in range(len(s))]
+        for i in range(len(s)):
+            if s[i] == ')' and i - 1 >= 0:
+                if s[i-1] == '(':
+                    valid[i] = i - 1
+                elif valid[i-1] > 0 and s[valid[i-1] - 1] == '(':
+                    valid[i] = valid[i-1] - 1
+                if valid[i] > 0 and valid[valid[i] - 1] >= 0:
+                    valid[i] = valid[valid[i] - 1]
+            if valid[i] >= 0 and i - valid[i] > result:
+                result = i - valid[i]
+        return result
