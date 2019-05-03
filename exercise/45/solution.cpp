be2e61281654005835f7fe66f4c2f47199636b5d
zizhazhu <zizhazhu@gmail.com>
2019-05-03 15:09:38 +0800

    fix bug

--- exercise/45/solution.cpp
+++ exercise/45/solution.cpp
@@ -5,15 +5,16 @@ public:
         if (n <= 1) return 0;
         int current = 0, pre = 0, jump = 0;
         for (int i = 0; i < n; i++) {
-            if (i + nums[i] >= n - 1)
-                return jump + 1;
             if (i > pre) {
                 jump++;
                 pre = current;
             }
+            if (i + nums[i] >= n - 1)
+                return jump + 1;
             if (i + nums[i] > current) {
                 current = i + nums[i];
             }
         }
+        return -1;
     }
 };
