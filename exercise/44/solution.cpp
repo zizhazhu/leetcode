92c48e48dc616e1f40d18fcc72460195c4e05717
zizhazhu <zizhazhu@gmail.com>
2019-05-03 11:56:09 +0800

    optimize

--- exercise/44/solution.cpp
+++ exercise/44/solution.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<bool>> match(s.length() + 1, vector<bool>(p.length() + 1, false));
-        match[0][0] = true;
+        vector<bool> match(p.length() + 1, false);
+        match[0] = true;
+        int pre, next_pre;
         for (int i = 0; i <= s.length(); i++) {
+            pre = match[0];
+            if (i > 0) match[0] = false;
             for (int j = 1; j <= p.length(); j++) {
-                if (i > 0 && match[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '?' || p[j-1] == '*')) {
-                    match[i][j] = true;
+                next_pre = match[j];
+                if (p[j-1] != '*') {
+                    match[j] = false;
                 }
-                if (i > 0 && match[i-1][j] && p[j-1] == '*') {
-                    match[i][j] = true;
+                if (match[j-1] && p[j-1] == '*') {
+                    match[j] = true;
                 }
-                if (match[i][j-1] && p[j-1] == '*') {
-                    match[i][j] = true;
+                if (i > 0 && pre && (s[i-1] == p[j-1] || p[j-1] == '?')) {
+                    match[j] = true;
                 }
+                pre = next_pre;
             }
         }
-        return match[s.length()][p.length()];
+        return match[p.length()];
     }
 };
