d1bbea2b4562e8ded9d2144f34bc95e3e802add4
zizhazhu <zizhazhu@gmail.com>
2019-05-03 15:09:11 +0800

    exercise 45 add cpp solution

---
+++ exercise/45/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) return 0;
+        int current = 0, pre = 0, jump = 0;
+        for (int i = 0; i < n; i++) {
+            if (i + nums[i] >= n - 1)
+                return jump + 1;
+            if (i > pre) {
+                jump++;
+                pre = current;
+            }
+            if (i + nums[i] > current) {
+                current = i + nums[i];
+            }
+        }
+    }
+};
