d36c1742f64a5dfccf404f320bb309d59d823d3d
zizhazhu <zizhazhu@gmail.com>
2019-05-08 10:08:39 +0800

    fix question understanding

--- exercise/53/solution.cpp
+++ exercise/53/solution.cpp
@@ -4,17 +4,13 @@ public:
         int result = 0;
         if (nums.empty()) return result;
         vector<int> f(nums.size());
-        if (nums[0] > 0)
-            f[0] = nums[0];
-        else
-            f[0] = 0;
+        f[0] = nums[0];
         result = f[0];
         for (int i = 1; i < nums.size(); i++) {
-            int now = nums[i] + f[i-1];
-            if (now > 0)
-                f[i] = now;
+            if (f[i-1] > 0)
+                f[i] = f[i-1] + nums[i];
             else
-                f[i] = 0;
+                f[i] = nums[i];
             if (f[i] > result) result = f[i];
         }
         return result;
