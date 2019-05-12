227bc45f97fd0d127d069882e08b72f33d47b5ec
zizhazhu <zizhazhu@gmail.com>
2019-05-12 17:57:12 +0800

    fix bug

--- exercise/685/solution.cpp
+++ exercise/685/solution.cpp
@@ -1,6 +1,8 @@
 struct UnionSet {
     vector<int> father;
-    UnionSet(int n): father(n) {}
+    UnionSet(): father(1001) {
+        for (int i = 0; i < father.size(); i++) father[i] = i;
+    }
     int root(int a) {
         if (father[a] != a) father[a] = root(father[a]);
         return father[a];
@@ -19,7 +21,7 @@ class Solution {
 public:
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        UnionSet union_set(n);
+        UnionSet union_set;
         for (int i = 0; i < n; i++) {
             int a = edges[i][0], b = edges[i][1];
             if (union_set.equal(a, b)) {
