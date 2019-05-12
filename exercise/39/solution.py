efb740ee6a23cacc392eea6c642ea4ae1a6fdafc
zizhazhu <zizhazhu@gmail.com>
2019-05-12 19:11:07 +0800

    fix bug

--- exercise/39/solution.py
+++ exercise/39/solution.py
@@ -1,17 +1,14 @@
 class Solution:
     def combinationSumPart(self, candidates, target, index, result, now):
         if target == 0:
-            result.append(now)
+            result.append(list(now))
         else:
             if index == len(candidates) or candidates[index] > target:
                 return
             now.append(candidates[index])
-            self.combinationSumPart(candidates, target - candidates[index], index + 1, result, now)
+            self.combinationSumPart(candidates, target - candidates[index], index, result, now)
             now.pop()
-            next_index = index + 1
-            while next_index < len(candidates) and candidates[next_index] == candidates[next_index-1]:
-                next_index += 1
-            self.combinationSumPart(candidates, target, next_index, result, now)
+            self.combinationSumPart(candidates, target, index+1, result, now)
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
