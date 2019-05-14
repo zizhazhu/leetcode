cd1b6012bfa3296f389b42d1fd513468e3da0d9e
zizhazhu <zizhazhu@gmail.com>
2019-05-14 14:16:00 +0800

    optimize

--- exercise/743/solution.cpp
+++ exercise/743/solution.cpp
@@ -8,33 +8,33 @@ public:
     }
 
     int dijkstra(int s) {
-        int result = 0;
-        priority_queue<pair<int, int>> queue;
+        vector<int> dis(edges.size(), 1000000);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> queue;
         unordered_set<int> reached;
         queue.push(make_pair(0, s));
-        for (int i = 1; i < edges.size(); i++) {
-            if (i != s) {
-                queue.push(make_pair(-1000000, i));
-            }
-        }
+        dis[s] = 0;
         while (!queue.empty()) {
             pair<int, int> now = queue.top();
             queue.pop();
             if (reached.count(now.second) != 0)
                 continue;
-            if (-now.first > result) {
-                result = -now.first;
-            }
             reached.insert(now.second);
             for (const pair<int, int> &it: edges[now.second]) {
-                if (reached.count(it.first) != 0)
-                    continue;
-                else
-                    queue.push(make_pair(-(-now.first + it.second), it.first));
+                if (dis[it.first] > dis[now.second] + it.second) {
+                    dis[it.first] = dis[now.second] + it.second;
+                    queue.push(make_pair(dis[it.first], it.first));
+                }
             }
         }
-        if (result == 1000000) return -1;
-        else return result;
+        int result = 0;
+        for (int i = 1; i < edges.size(); i++) {
+            if (dis[i] == 1000000) {
+                return -1;
+            } else if (dis[i] > result) {
+                result = dis[i];
+            }
+        }
+        return result;
     }
 };
 
