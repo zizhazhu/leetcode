70d013b2cc4dbfc9560d486f2491cd15405a2c2d
zizhazhu <zizhazhu@gmail.com>
2019-05-14 14:42:40 +0800

    fix bug

--- exercise/44/solution.py
+++ exercise/44/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        match = [False for i in range(len(s) + 1)]
+        match = [False for i in range(len(p) + 1)]
         match[0] = True
         pre = False
         for i in range(len(s) + 1):
@@ -10,13 +10,12 @@ class Solution:
             for j in range(1, len(p) + 1):
                 new_pre = match[j]
                 match[j] = False
-                if pre and i > 0 and (s[i-1] == p[j-1] or p[j-1] == '.'):
+                if pre and i > 0 and (s[i-1] == p[j-1] or p[j-1] == '?'):
                     match[j] = True
                 elif match[j-1] and p[j-1] == '*':
                     match[j] = True
                 elif i > 0 and new_pre and p[j-1] == '*':
                     match[j] = True
                 pre = new_pre
-        return match[p+1]
-
+        return match[len(p)]
 
