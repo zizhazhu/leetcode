9227ac8d52a01e91d5252c23e502eee60490d1b1
zizhazhu <zizhazhu@gmail.com>
2019-05-15 09:32:07 +0800

    exercise 45 add python solution

---
+++ exercise/45/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        edge = 0
+        steps = [0 for _ in range(len(nums))] 
+        for i in range(len(nums)):
+            if i + nums[i] >= len(nums) - 1:
+                return steps[i] + 1
+            elif i + nums[i] > edge:
+                for j in range(edge + 1, i + nums[i] + 1):
+                    steps[j] = steps[i] + 1
+        return steps[-1]
