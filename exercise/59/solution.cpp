43be122989527596bebba05a17f13a24f0cb75d8
zizhazhu <zizhazhu@gmail.com>
2019-05-22 17:01:17 +0800

    exercise 59 add cpp solution

--- exercise/59/solution.cpp
+++ exercise/59/solution.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        
+        vector<vector<int>> result;
+        int way[][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int now = 1, index_x = 0, index_y = 0, direction = 0;
+        int all = n * n;
+        while (now <= all) {
+            result[index_x][index_y] = now;
+            int next_x = index_x + way[direction][0], next_y = index_y + way[direction][1];
+            if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= n || result[next_x][next_y] != 0) {
+                direction = (direction + 1) % 4;
+                next_x = index_x + way[direction][0];
+                next_y = index_y + way[direction][1];
+            }
+            index_x = next_x, index_y = next_y;
+            now++;
+        }
+        return result;
     }
 };
