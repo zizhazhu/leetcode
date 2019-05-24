5bfa7452cdbd6df29bd4bb41b1383ca5513bd33f
zizhazhu <zizhazhu@gmail.com>
2019-05-24 10:55:29 +0800

    exercise 61 add cpp solution

--- exercise/61/solution.cpp
+++ exercise/61/solution.cpp
@@ -9,6 +9,26 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        
+        if (!head || k == 0) return head;
+        ListNode *p = head;
+        int length = 0;
+        while (p) {
+            length++;
+            p = p->next;
+        }
+        int res_k = k % length;
+        if (res_k == 0) return head;
+        p = head;
+        for (int i = 0; i < res_k; i++)
+            p = p->next;
+        ListNode *tail = head;
+        while (p->next) {
+            p = p->next;
+            tail = tail->next;
+        }
+        p->next = head;
+        head = tail->next;
+        tail->next = NULL;
+        return head;
     }
 };
