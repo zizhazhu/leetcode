8693c4290380cba3a05be2d2edfd2387df58984c
zizhazhu <zizhazhu@gmail.com>
2019-06-12 10:47:01 +0800

    exercise 64 add cpp solution

--- exercise/64/solution.cpp
+++ exercise/64/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        
+        unsigned m = grid.size();
+        if (m == 0) return 0;
+        unsigned n = grid[0].size();
+        if (n == 0) return 0;
+        vector<int> f(n, 0);
+        for (int i = 0; i < m; i++) {
+            f[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                f[j] = min(f[j], f[j-1]) + grid[i][j];
+            }
+        }
+        return f[n-1];
     }
 };
