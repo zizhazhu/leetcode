5e3aa4de3ae27460eda0a1d282dadfce240f024f
zizhazhu <zizhazhu@gmail.com>
2019-06-12 10:50:30 +0800

    fix bug

--- exercise/64/solution.cpp
+++ exercise/64/solution.cpp
@@ -5,8 +5,12 @@ public:
         if (m == 0) return 0;
         unsigned n = grid[0].size();
         if (n == 0) return 0;
-        vector<int> f(n, 0);
-        for (int i = 0; i < m; i++) {
+        vector<int> f(n);
+        f[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            f[i] = f[i-1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
             f[0] += grid[i][0];
             for (int j = 1; j < n; j++) {
                 f[j] = min(f[j], f[j-1]) + grid[i][j];
