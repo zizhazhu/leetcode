9d6082e3228ee330f6195f1f76f015b4a659f8fe
zizhazhu <zizhazhu@gmail.com>
2019-06-19 10:24:19 +0800

    fix bug

--- exercise/66/solution.cpp
+++ exercise/66/solution.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> result(digits.size());
         int carry = 1;
         for (int i = digits.size() - 1; i >= 0; i--) {
-            int result = digits[i] + carry;
-            result[i] = result % 10;
-            carry = result / 10;
+            int temp = digits[i] + carry;
+            result[i] = temp % 10;
+            carry = temp / 10;
         }
         if (carry == 1) {
             result.insert(result.begin(), carry);
