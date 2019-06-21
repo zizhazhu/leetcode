30d29282772bb9e764519177d2091755ce685a76
zizhazhu <zizhazhu@gmail.com>
2019-06-21 10:34:43 +0800

    exercise 69 add python solution

---
+++ exercise/69/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x // 2
+        while left < right:
+            mid = (left + right + 1) // 2
+            square_mid = mid * mid
+            if square_mid == x:
+                return mid
+            elif square_mid < x:
+                left = mid
+            else:
+                right = mid - 1
+        return left
