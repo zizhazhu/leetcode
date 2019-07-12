6e6d4efe3932f161c262fe1f6689cfef271eb960
zizhazhu <zizhazhu@gmail.com>
2019-07-12 10:23:49 +0800

    fix bug

--- exercise/73/solution.py
+++ exercise/73/solution.py
@@ -18,7 +18,8 @@ class Solution:
                 first_col = True
         for i in range(1, m):
             for j in range(1, n):
-                matrix[i][0] = matrix[0][j] = 0
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
         for i in range(1, m):
             if matrix[i][0] == 0:
                 for j in range(1, n):
