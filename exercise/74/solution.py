db097a230c4e586090f030e566d711d92ce376b6
zizhazhu <zizhazhu@gmail.com>
2019-07-31 10:43:12 +0800

    exercise 74 add python solution

---
+++ exercise/74/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        if n == 0:
+            return False
+        left, right = 0, m
+        while left < right:
+            mid = left + (right - left) // 2
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] < target:
+                left = mid
+            else:
+                right = mid
+        row = left
+        if row == m:
+            return False
+        left, right = 0, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return False
