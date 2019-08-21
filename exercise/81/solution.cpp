9092579c5295e37ed05f6a4cbd407575d830e2d0
zizhazhu <zizhazhu@gmail.com>
2019-08-21 11:24:09 +0800

    exercise 81 add cpp solution

---
+++ exercise/81/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] == nums[left]) {
+                left++;
+            } else if (nums[mid] == nums[right]) {
+                right--;
+            }
+            if (nums[mid] < nums[left]) {
+                if (target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    if (target > nums[right]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+            } else if (nums[mid] > nums[left]) {
+                if (target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    if (target < nums[left]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
