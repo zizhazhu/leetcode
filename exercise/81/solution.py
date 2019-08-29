3e972a1e88e4239929b5e224b39150ad0e448b39
zizhazhu <zizhazhu@gmail.com>
2019-08-29 12:03:28 +0800

    fix bug

--- exercise/81/solution.py
+++ exercise/81/solution.py
@@ -12,14 +12,20 @@ class Solution:
             elif nums[mid] == nums[right]:
                 right -= 1
             elif target < nums[mid]:
-                if target >= nums[left]:
-                    right = mid - 1
+                if nums[mid] > nums[left]:
+                    if target >= nums[left]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
                 else:
-                    left = mid + 1
+                    right = mid - 1
             else:
-                if target >= nums[left]:
+                if nums[mid] > nums[left]:
                     left = mid + 1
                 else:
-                    right = mid - 1
+                    if target >= nums[left]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
         return False
                     
