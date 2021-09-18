d3dca3a82af7a0aaceef55456770d2e49757cbee
zizhazhu <zizhazhu@gmail.com>
2021-09-18 14:33:58 +0800

    optimize 417

--- exercise/BFS/417.太平洋大西洋水流问题.py
+++ exercise/BFS/417.太平洋大西洋水流问题.py
@@ -9,39 +9,46 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         result = []
         m, n = len(heights), len(heights[0])
+        p, w = set(), set()
 
-        def BFS(x, y):
-            from collections import deque
-            queue = deque()
-            reached = set()
-            queue.append((x, y))
-            reached.add((x, y))
+        from collections import deque
+        queue = deque()
+        queue.append((0, 0))
+        p.add((0, 0))
+        for i in range(1, m):
+            queue.append((i, 0))
+            p.add((i, 0))
+        for j in range(1, n):
+            queue.append((0, j))
+            p.add((0, j))
+
+        def BFS(r_set):
             way = ((-1, 0), (1, 0), (0, -1), (0, 1))
-            p, w = False, False
 
             while len(queue) > 0:
-                if p and w:
-                    return p, w
                 i, j = queue.popleft()
-                if i == 0 or j == 0:
-                    p = True
-                if i == m - 1 or j == n - 1:
-                    w = True
                 for i_delta, j_delta in way:
                     new_i = i + i_delta
                     new_j = j + j_delta
-                    if new_i >= 0 and new_i < m and new_j >= 0 and new_j < n and heights[new_i][new_j] <= heights[i][j] and (new_i, new_j) not in reached:
+                    if 0 <= new_i < m and 0 <= new_j < n and heights[new_i][new_j] >= heights[i][j] and (new_i, new_j) not in r_set:
                         queue.append((new_i, new_j))
-                        reached.add((new_i, new_j))
+                        r_set.add((new_i, new_j))
                     
-            return p, w
+        BFS(p)
 
-        for i in range(m):
-            for j in range(n):
-                p, w = BFS(i, j)
-                if p and w:
-                    result.append([i, j])
-
+        queue.clear()
+        queue.append((m - 1, n - 1))
+        w.add((m - 1, n - 1))
+        for i in range(0, m - 1):
+            queue.append((i, n - 1))
+            w.add((i, n - 1))
+        for j in range(0, n - 1):
+            queue.append((m - 1, j))
+            w.add((m - 1, j))
+        BFS(w)
+        for x, y in p:
+            if (x, y) in w:
+                result.append([x, y])
         return result
 # @lc code=end
 
