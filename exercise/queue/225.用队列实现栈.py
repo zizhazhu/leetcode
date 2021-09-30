30e30406fbb1686f06f083795f421b6b0c42cb27
zizhazhu <zizhazhu@gmail.com>
2021-09-30 14:08:36 +0800

    add 225

---
+++ exercise/queue/225.用队列实现栈.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode.cn id=225 lang=python3
+#
+# [225] 用队列实现栈
+#
+
+# @lc code=start
+class MyStack:
+
+    def __init__(self):
+        import collections
+        self.queue = collections.deque()
+
+    def push(self, x: int) -> None:
+        n = len(self.queue)
+        self.queue.append(x)
+        for i in range(n):
+            self.queue.append(self.queue.popleft())
+
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+
+    def top(self) -> int:
+        return self.queue[0]
+
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+# @lc code=end
+
