aa3c91e49dd383c404a08761d0955bffd0380883
zizhazhu <zizhazhu@gmail.com>
2021-10-18 14:04:23 +0800

    optimize 119

--- exercise/Math/119.杨辉三角-ii.py
+++ exercise/Math/119.杨辉三角-ii.py
@@ -9,19 +9,9 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         result = [1]
 
-        def fact(begin, n):
-            result = 1
-            for i in range(n):
-                result *= begin
-                begin -= 1
-            return result
-
-        for i in range(1, rowIndex // 2 + 1):
-            num = fact(rowIndex, i) // fact(i, i)
-            result.append(num)
-
-        for i in range((rowIndex - 1) // 2, -1, -1):
-            result.append(result[i])
+        for i in range(1, rowIndex + 1):
+            last = result[-1]
+            result.append(last * (rowIndex - i + 1) // i)
         
         return result
 
